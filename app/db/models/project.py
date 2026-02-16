f8078eb4ec16614f7376a6f292a5f15610b4cc90
Srinivas Bhuvanagiri <32320907+sri2850@users.noreply.github.com>
2026-02-17 00:39:01 +0530

    added created_by column

--- app/db/models/project.py
+++ app/db/models/project.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String
+from sqlalchemy import ForeignKey, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.db.base import Base
@@ -8,3 +8,8 @@ class Project(Base):
     __tablename__ = "projects"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(100), unique=True)
+    created_by: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("users.id"),
+        nullable=False,
+    )
--- app/db/models/user.py
+++ app/db/models/user.py
@@ -5,7 +5,7 @@ from app.db.base import Base
 
 
 class User(Base):
-    __tablename__ = "user"
+    __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(String(100), unique=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
