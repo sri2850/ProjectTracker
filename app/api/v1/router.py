16daffaa2e4608674a6fb648d619f9be68c582b2
Srinivas Bhuvanagiri <32320907+sri2850@users.noreply.github.com>
2026-02-22 20:15:54 +0530

    Added user router and tested

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter
 
 from app.api.v1.endpoints.auth import router as auth_router
 from app.api.v1.endpoints.project import router as project_router
+from app.api.v1.endpoints.user import router as user_router
 
 router = APIRouter(prefix="/api/v1")
 router.include_router(project_router, tags=["projects"])
 router.include_router(auth_router, tags=["auth"])
+router.include_router(user_router, tags=["user"])
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,12 +1,6 @@
 from pydantic import BaseModel, EmailStr
 
 
-class User(BaseModel):
-    username: str
-    email: str | None = None
-    disabled: bool | None = None
-
-
 class UserCreate(BaseModel):
     username: str
     email: EmailStr
